bb84e6f7280781545f500f054832beffeac0e484
shadinmhd <shadinmhd97@gmail.com>
2023-06-09 19:36:25 +0530

    made some simple changes in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Game from './components/Game'
+import Taskbar from './components/Taskbar'
 
 function App() {
   const [play, setplay] = useState(true)
   const [input, setInput] = useState("X")
   return( <>
-  { play? (
+  { play? (<>
+    <h1>Tic Tac Toe</h1>
     <Game play={play} setplay={setplay} input={input} setInput={setInput} />
+  </>
   ) : (
     <h1>Some one Won</h1>
   )}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,71 +3,72 @@ import '../App.css'
 
 function Game(prop) {
     const [val, setVal] = useState([
-        {key : 1, label : " "},
-        {key : 2, label : " "},
-        {key : 3, label : " "},
-        {key : 4, label : " "},
-        {key : 5, label : " "},
-        {key : 6, label : " "},
-        {key : 7, label : " "},
-        {key : 8, label : " "},
-        {key : 9, label : " "}
-      ])
-    
-      function checkWinner(){
-        if(prop.play){
-          if(val[0].label === val[1].label && val[1].label === val[2].label && val[2].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[3].label === val[4].label && val[4].label === val[5].label && val[5].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[6].label === val[7].label && val[7].label === val[8].label && val[8].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[0].label === val[3].label && val[3].label === val[6].label && val[6].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[1].label === val[4].label && val[4].label === val[7].label && val[7].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[2].label === val[5].label && val[5].label === val[8].label && val[8].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[0].label === val[4].label && val[4].label === val[8].label && val[8].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
-          else if(val[2].label === val[4].label && val[4].label === val[6].label && val[6].label != " "){
-            console.log("i think")
-            prop.setplay(false)
-          }
+        { key: 1, label: " " },
+        { key: 2, label: " " },
+        { key: 3, label: " " },
+        { key: 4, label: " " },
+        { key: 5, label: " " },
+        { key: 6, label: " " },
+        { key: 7, label: " " },
+        { key: 8, label: " " },
+        { key: 9, label: " " }
+    ])
+
+    function checkWinner() {
+        if (prop.play) {
+            if (val[0].label === val[1].label && val[1].label === val[2].label && val[2].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[3].label === val[4].label && val[4].label === val[5].label && val[5].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[6].label === val[7].label && val[7].label === val[8].label && val[8].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[0].label === val[3].label && val[3].label === val[6].label && val[6].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[1].label === val[4].label && val[4].label === val[7].label && val[7].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[2].label === val[5].label && val[5].label === val[8].label && val[8].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[0].label === val[4].label && val[4].label === val[8].label && val[8].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
+            else if (val[2].label === val[4].label && val[4].label === val[6].label && val[6].label != " ") {
+                console.log("i think")
+                prop.setplay(false)
+            }
         }
-      }
-    
-      function click(key){
+    }
+
+    function click(key) {
         let temp = [...val]
-        if(temp[key-1].label === " ") {
-          temp[key-1].label = prop.input
-          prop.input === "X" ? prop.setInput("O") : prop.setInput("X")
+        if (temp[key - 1].label === " ") {
+            temp[key - 1].label = prop.input
+            prop.input === "X" ? prop.setInput("O") : prop.setInput("X")
         }
         setVal(temp)
         checkWinner()
-      }
-      return (<>
+    }
+    return (
+    <div id='Game'>
         <div id='container' >
-          {val.map((e) => {
-            return <button disabled={prop.play? false : true} key={e.key} onClick={() => click(e.key)} className='btn'>{e.label}</button>
-          })}
+            {val.map((e) => {
+                return <button disabled={prop.play ? false : true} key={e.key} onClick={() => click(e.key)} className='btn'>{e.label}</button>
+            })}
         </div>
-      </>
-      )
+    </div>
+    )
 }
 
 export default Game
